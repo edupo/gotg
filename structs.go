fbd6a2e80039df184ffa54cd113b3cb1418f6bdf
Eduardo Lezcano <contact@eduardolezcano.com>
2018-08-18 19:33:44 +0200

    Peer and channel structs

--- structs.go
+++ structs.go
@@ -1,13 +1,24 @@
 package gotg
 
-type Contact struct {
+type Peer struct {
 	Id        string `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
 	PrintName string `json:"print_name"`
+	Flags     uint64 `json:"flags"`
 	PeerType  string `json:"peer_type"`
 	PeerId    uint64 `json:"peer_id"`
-	Flags     uint64 `json:"flags"`
+}
+
+type Contact struct {
+	Peer
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 	When      string `json:"when"`
 	Phone     string `json:"phone"`
+}
+
+type Channel struct {
+	Peer
+	Title       string `json:"title"`
+	AdminsCount uint64 `json:"admins_count"`
+	KickedCount uint64 `json:"kicked_count"`
 }
